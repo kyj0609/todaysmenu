15237659bcd6144d21ff2766d84a9dc0d1bb24d9
Juun <kyj0609@knu.ac.kr>
2019-11-08 15:15:04 +0900

    kyj

--- app/src/main/java/com/example/todaymenue/MainActivity.java
+++ app/src/main/java/com/example/todaymenue/MainActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // 어플을 실행하면, manifest에서 MainActivity 클래스를 시작시키고, MainActivity클래스의 onCreate()함수가
+    // activity_main_xml 파일을 읽어서 화면에 레이아웃을 그려 안드로이드 기기 화면에 보여준다.
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) {// 프로그램의 시작점
+        super.onCreate(savedInstanceState); //부모 클래스의 onCreate()함수를 상속받아 실행한다.
+        setContentView(R.layout.activity_main);// reg<layout> activity_main.xml를 호출한것
     }
 }
